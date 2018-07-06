890060fe2d3fae570b805d9942cbd5ec22046671
DAVIS Elliott <davis@team-lab.com>
2018-07-06 13:35:49 +0900

    adding some network logging

--- Assets/Packages/SyncUtil/Scripts/SyncNetworkManager.cs
+++ Assets/Packages/SyncUtil/Scripts/SyncNetworkManager.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine.Networking;
+using UnityEngine;
 
 namespace SyncUtil
 {
@@ -39,6 +40,7 @@ namespace SyncUtil
         public event System.Action<NetworkClient> _OnStartClient = delegate { };
         public override void OnStartClient(NetworkClient client)
         {
+			Debug.LogFormat("{0} Client networking logic is starting", System.DateTime.Now.TimeOfDay);
             base.OnStartClient(client);
             _OnStartClient(client);
         }
@@ -46,6 +48,7 @@ namespace SyncUtil
         public event System.Action<NetworkConnection> _OnClientConnect = delegate { };
         public override void OnClientConnect(NetworkConnection conn)
         {
+			Debug.LogFormat("{0} Client connection ID: {1}  has connected to the server", System.DateTime.Now.TimeOfDay, conn.connectionId);
             base.OnClientConnect(conn);
             _OnClientConnect(conn);
         }
@@ -54,9 +57,38 @@ namespace SyncUtil
         public event System.Action<NetworkConnection, int> _OnClientError = delegate { };
         public override void OnClientError(NetworkConnection conn, int errorCode)
         {
+			Debug.LogErrorFormat("{0} Client Error: connection ID: {1}  error code: {2} error message: {3} ",System.DateTime.Now.TimeOfDay, conn.connectionId, errorCode, NetworkErrorToString(errorCode));
             base.OnClientError(conn, errorCode);
             _OnClientError(conn, errorCode);
         }
-        #endregion
-    }
+
+		public override void OnClientDisconnect(NetworkConnection conn)
+		{
+			Debug.LogWarningFormat("{0} Client Disconeect  connection ID {1}", System.DateTime.Now.TimeOfDay, conn.connectionId);
+			base.OnClientDisconnect(conn);
+		}
+		#endregion
+
+		string NetworkErrorToString(int networkErrorID)
+		{
+			switch ((NetworkError)networkErrorID)
+			{
+				case NetworkError.Ok:					return "OK. no error!";
+				case NetworkError.WrongHost:			return "wrong host";
+				case NetworkError.WrongConnection:		return "wrong connection";
+				case NetworkError.WrongChannel:			return "wrong Channel";
+				case NetworkError.NoResources:			return "no resources";
+				case NetworkError.BadMessage:			return "bad message";
+				case NetworkError.Timeout:				return "timeout";
+				case NetworkError.MessageToLong:		return "message to long";
+				case NetworkError.WrongOperation:		return "wrong operation";
+				case NetworkError.VersionMismatch:		return "version mismatch";
+				case NetworkError.CRCMismatch:			return "CRC mismatch";
+				case NetworkError.DNSFailure:			return "DNS failure";
+				case NetworkError.UsageError:			return "usage error";
+				default:								return string.Format("unknown error code ({0})", networkErrorID);
+			}
+		
+		}
+	}
 }
