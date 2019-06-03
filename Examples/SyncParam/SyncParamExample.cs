aec5052496c40b74f4c9e1df2f5ddb50c774debb
fuqunaga <fukhid@gmail.com>
2019-06-03 16:19:59 +0900

    upgrade unity2019

--- Examples/SyncParam/SyncParamExample.cs
+++ Examples/SyncParam/SyncParamExample.cs
@@ -21,7 +21,11 @@ namespace SyncUtil.Example
 
         private void OnDisable()
         {
-            DebugMenuForExample.Instance.onGUI -= DebugMenu;
+            var menu = DebugMenuForExample.Instance;
+            if (menu != null)
+            {
+                menu.onGUI -= DebugMenu;
+            }
         }
 
         public void DebugMenu()
