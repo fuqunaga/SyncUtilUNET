fa9e5ece31b54143c778cfad331b0913a3543b8a
DavisTeamLab <davis@team-lab.com>
2019-11-27 19:28:50 +0900

    updating NetworkManagerControler GUI.

--- Scripts/NetworkManagerController.cs
+++ Scripts/NetworkManagerController.cs
@@ -95,25 +95,33 @@ namespace SyncUtil
             {
                 GUILayout.Label("SyncUtil Manual Boot");
 
-                GUIUtil.Indent(() =>
+                //GUIUtil.Indent(() =>
                 {
                     var mgr = _networkManager;
-                    mgr.useSimulator = GUILayout.Toggle(mgr.useSimulator, "UseSimulator");
-                    if (mgr.useSimulator)
-                    {
-                        GUIUtil.Indent(() =>
-                        {
-                            mgr.simulatedLatency = GUIUtil.Slider(mgr.simulatedLatency, 1, 400, "Latency[msec]");
-                            mgr.packetLossPercentage = GUIUtil.Slider(mgr.packetLossPercentage, 0f, 20f, "PacketLoss[%]");
-                        });
-                    }
+
 
 					OnGUINetworkSetting();
 					GUILayout.Label("Boot Type (Manual. once only):");
                     if (GUILayout.Button("Host (client & server)")) { OnNetworkStartByManual(); StartNetwork(BootType.Host); }
                     if (GUILayout.Button("Client")) { OnNetworkStartByManual(); StartNetwork(BootType.Client); }
-                    if (GUILayout.Button("Server")) { OnNetworkStartByManual(); StartNetwork(BootType.Server); }
-                });
+                    if (GUILayout.Button("Server"))
+                    {
+                        OnNetworkStartByManual();
+                        StartNetwork(BootType.Server);
+                    }
+
+                    GUILayout.Label("");
+                    mgr.useSimulator = GUILayout.Toggle(mgr.useSimulator, "Use Network Simulator");
+                    if (mgr.useSimulator)
+                    {
+                        //GUIUtil.Indent(() =>
+                        {
+                            mgr.simulatedLatency = GUIUtil.Slider(mgr.simulatedLatency, 1, 400, "Latency[msec]");
+                            mgr.packetLossPercentage = GUIUtil.Slider(mgr.packetLossPercentage, 0f, 20f, "PacketLoss[%]");
+                            GUILayout.Label("");
+                        }//);
+                    }
+                }//);
             }
         }
 
