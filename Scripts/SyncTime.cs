34925a47b006ebe0c734c7ff37e4bc5f76041722
DavisTeamLab <davis@team-lab.com>
2019-12-16 13:35:29 +0900

    fixing a bug where if the server is restarted, clients will no longer sync the time setting sent by the server because the client's _LastMsg.time value is higher than the restarted sever's message time value.

--- Scripts/SyncTime.cs
+++ Scripts/SyncTime.cs
@@ -58,6 +58,12 @@ namespace SyncUtil
                     StartCoroutine(UpdateTimeClient());
                 }
             };
+            // in case the server restarts, when the client next connects the server, make sure the client's last message is reset to null, 
+            // otherwise in a rare case when the server app restarts, the server's lastMsg.time will be less than the client's lastMsg.time and time chnages on the server will not sync properly on the client.
+            SyncNetworkManager.singleton._OnClientConnect += (networkConn) =>
+            {
+                _lastMsg = null;
+            };
         }
 
         public void Update()
