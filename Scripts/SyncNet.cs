fde6bbba51efb76b21f71a16f7b2f1ed992f7040
fuqunaga <fukhid@gmail.com>
2020-01-21 18:25:00 +0900

    cosmetic

--- Scripts/SyncNet.cs
+++ Scripts/SyncNet.cs
@@ -7,18 +7,18 @@ namespace SyncUtil
 {
     public class SyncNet
     {
-        public static bool isServer { get { return NetworkServer.active; } }
-        public static bool isClient { get { return NetworkClient.active; } }
-        public static bool isHost { get { return isServer && isClient; } }
-        public static bool isStandAlone { get { return !isServer && !isClient; } }
+        public static bool isServer => NetworkServer.active;
+        public static bool isClient => NetworkClient.active;
+        public static bool isHost => isServer && isClient;
+        public static bool isStandAlone => !isServer && !isClient;
 
-        public static bool isServerOrStandAlone { get { return isServer || !isClient; } }
+        public static bool isServerOrStandAlone => isServer || !isClient;
 
         // Not Server but Client.
         // warn Host: isServer == isClient == true
-        public static bool isSlave { get { return !isServerOrStandAlone; } }
+        public static bool isSlave => !isServerOrStandAlone;
 
-        public static bool isActive { get { var nm = NetworkManager.singleton; return (nm !=null) && nm.isNetworkActive; } }
+        public static bool isActive { get { var nm = NetworkManager.singleton; return (nm != null) && nm.isNetworkActive; } }
 
         public static NetworkClient client { get { var nm = NetworkManager.singleton; return (nm == null) ? null : nm.client; } }
 
@@ -35,7 +35,7 @@ namespace SyncUtil
         }
 
 
-        public static float time { get { return SyncTime.Instance.time; } }
-        public static double networkTime { get { return SyncNetworkTime.Instance.time; } }
+        public static float time => SyncTime.Instance.time;
+        public static double networkTime => SyncNetworkTime.Instance.time;
     }
 }
